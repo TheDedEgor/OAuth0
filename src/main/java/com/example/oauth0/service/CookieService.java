5d745235fd828a3752522a2d5591597ca8fd9887
Egor <korepin.02@gmail.com>
2025-12-28 01:06:06 +0300

    убираем защиту

--- src/main/java/com/example/oauth0/service/CookieService.java
+++ src/main/java/com/example/oauth0/service/CookieService.java
@@ -8,8 +8,8 @@ import java.time.ZonedDateTime;
 public class CookieService {
     public static Cookie createForSecureToken(String secureToken, ZonedDateTime expiredAt) {
         var cookie = new Cookie("OAUTH_SECURE_TOKEN", secureToken);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
         cookie.setPath("/");
         var seconds = Duration.between(ZonedDateTime.now(), expiredAt).getSeconds();
         cookie.setMaxAge((int) seconds);
