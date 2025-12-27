86e8fba4571c710c0ebb972a522f139f9abe9b27
Egor <korepin.02@gmail.com>
2025-12-28 01:18:48 +0300

    тест

--- src/main/java/com/example/oauth0/service/CookieService.java
+++ src/main/java/com/example/oauth0/service/CookieService.java
@@ -9,7 +9,7 @@ public class CookieService {
     public static Cookie createForSecureToken(String secureToken, ZonedDateTime expiredAt) {
         var cookie = new Cookie("OAUTH_SECURE_TOKEN", secureToken);
 //        cookie.setHttpOnly(true);
-//        cookie.setSecure(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setAttribute("SameSite", "None");
         var seconds = Duration.between(ZonedDateTime.now(), expiredAt).getSeconds();
